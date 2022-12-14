e25b2d21788ba7e00d92176dc9e28e8f49e0cffb
Sayan Datta <hello@sayandatta.in>
2022-12-14 19:09:01 +0530

    added css

--- build/newsletter-form/index.asset.php
+++ build/newsletter-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '4edc25fdfd56e88e9c63');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '37dbdb602ffb624dd5ba');
